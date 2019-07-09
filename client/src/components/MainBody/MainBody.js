5fa9edba846a6146cccd3eafb0d98c759254df14
Darkangel <wtsegars@mac.com>
2019-07-08 20:53:23 -0400

    Added images to the main menu

--- client/src/components/MainBody/MainBody.js
+++ client/src/components/MainBody/MainBody.js
@@ -13,12 +13,15 @@ class MainBody extends React.Component {
                     <div className="row">
                         <div className="col-sm">
                             View All Questionnaires
+                            <img src={require("./images/eye.jpg")} />
                         </div>
                         <div className="col-sm">
                             Add a New Questionnaire
+                            <img src={require("./images/plus-sign.png")} />
                         </div>
                         <div className="col-sm">
                             Edit a Questionnaire
+                            <img src={require("./images/tools.png")} />
                         </div>
                     </div>
                 </div>
