c19806a0ecfbc899221cf2fe95ff62d32258dac2
Darkangel <wtsegars@mac.com>
2019-07-10 19:43:15 -0400

    updated App.js to not include the flowchart

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Main from './pages/Main';
-import Flowchart from './pages/FlowChart';
 import NotFound from './pages/NotFound';
 import Questionnaire from './pages/Questionnaire';
 
@@ -17,7 +16,6 @@ function App() {
           <Redirect exact from="/login" to="/" />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/index" component={Main} />
-          <Route exact path="/flowchart/:id" component={Flowchart} />
           <Route exact path="/questionnaire" component={Questionnaire}/>
           <Route component={NotFound} />
         </Switch>
