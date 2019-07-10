298236a17b724b5e01dde353b20a967c26bf3a96
Darkangel <wtsegars@mac.com>
2019-07-10 18:39:15 -0400

    Changed the mainbody component to be a function

--- client/src/components/MainBody/MainBody.js
+++ client/src/components/MainBody/MainBody.js
@@ -1,29 +1,24 @@
 import React from 'react';
 
-class MainBody extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <h2>
-                    Welcome 
+const MainBody = () => (
+    <div>
+        <h2>
+            Welcome
                 </h2>
-                <div className="new">
-                    <form>
-                        <div className="form-group">
-                            <label for="newQuestionnaire">
-                                Add New Questionnaire
+        <div className="new">
+            <form>
+                <div className="form-group">
+                    <label for="newQuestionnaire">
+                        Add New Questionnaire
                             </label>
-                            <input className="form-control" id="newQuestionnaire" placeholder="Enter the name of your questionnaire here" />
-                            <button type="submit" className="btn btn-primary">
-                                <img src={require('./images/plus-sign.png')} />
-                            </button>
-                        </div>
-                    </form>
+                    <input className="form-control" id="newQuestionnaire" placeholder="Enter the name of your questionnaire here" />
+                    <button type="submit" className="btn btn-primary">
+                        <img src={require('./images/plus-sign.png')} />
+                    </button>
                 </div>
-            </div>
-        );
-    }
-}
+            </form>
+        </div>
+    </div>
+);
 
 export default MainBody;
