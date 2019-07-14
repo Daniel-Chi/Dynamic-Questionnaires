e6cb0ca35e18ae30aa84cf0ca68feb75059fa630
jlb99 <bivins99@bellsouth.net>
2019-07-14 15:33:32 -0400

    commit files

--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    userController: require('./userController'),
-    questionController: require('./questionController'),
-    flowchartController: require("./flowchartController"),
-    answersController: require("./answersController")
-};
