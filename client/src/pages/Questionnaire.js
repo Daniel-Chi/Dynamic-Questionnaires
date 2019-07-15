74dab38ce78ed83dd2dd2ea6c126c89027a754b9
Darkangel <wtsegars@mac.com>
2019-07-15 16:41:07 -0400

    Minor structure changes

--- client/src/pages/Questionnaire.js
+++ client/src/pages/Questionnaire.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import FormContainer from "../components/FormContainer/FormContainer"
 import AddButton from "../components/AddButton/AddButton"
-//import TextArea from "../components/TextArea/TextArea"
-//import TextRadio from "../components/TextRadio";
 
 function Questionnaire() {
     return (
         <React.Fragment>
             <FormContainer/>
             <AddButton/>
-            
-            
         </React.Fragment>
     )
 };
