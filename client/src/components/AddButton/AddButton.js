b585011e230122ecd4f4381f79e680bd8ae1e8df
Daniel Chi <danielhchi@gmail.com>
2019-07-18 17:41:44 -0400

    fixed formatting on Add btn and moved on click listener to a tag instead of img

--- client/src/components/AddButton/AddButton.js
+++ client/src/components/AddButton/AddButton.js
@@ -13,9 +13,20 @@ class AddButton extends Component {
     render() {
         return (
             <div>
-                <a href={window.location.pathname} data-toggle="tooltip" data-placement="bottom" title="Click here to add a new question :)">
-                    <img src={require("./add.png")} height="80px" id="add-img" alt="add" onClick={this.props.onClick}></img>
-                </a> 
+                <a
+                    href={window.location.pathname}
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title="Click here to add a new question :)"
+                    onClick={this.props.onClick}
+                >
+                    <img
+                        src={require("./add.png")}
+                        height="80px"
+                        id="add-img"
+                        alt="add"
+                    ></img>
+                </a>
             </div>
         );
     };
