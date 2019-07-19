04601312a2a1255c1ac5a97d86d6df97348493ea
jlb99 <bivins99@bellsouth.net>
2019-07-19 06:08:40 -0400

    don't think this commit is a change

--- routes/api/flowcharts.js
+++ routes/api/flowcharts.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const flowchartController = require("../../controllers/flowchartController");
 
 
-router.route("/api/flowcharts/?question=:id")
+router.route("/api/flowcharts/?user=:id")
  //.post(flowchartController.createNewAnswers);
  .post(flowchartController.createNewForm);
  
-router.route("/api/flowcharts/?question=:id")
+router.route("/api/flowcharts/?user=:id")
  .get(flowchartController.findAllForms);
 
 
